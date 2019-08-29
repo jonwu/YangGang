7054aac9138b6caf3efa611c9cd630c3fcf184fc
Jonwu <jonwu92@gmail.com>
2019-08-29 01:11:58 -0700

    fix regex

--- yang-gang-app/src/components/items/TwitterItem.js
+++ yang-gang-app/src/components/items/TwitterItem.js
@@ -82,22 +82,25 @@ const TwitterItem = ({ item, navigation }) => {
     return a.indices[0] > b.indices[0];
   });
   full_text = xmlEntities.decode(full_text);
+  // const regex = /(?:[\u2700-\u27bf]|(?:\ud83c[\udde6-\uddff]){2}|[\ud800-\udbff][\udc00-\udfff]|[\u0023-\u0039]\ufe0f?\u20e3|\u3299|\u3297|\u303d|\u3030|\u24c2|\ud83c[\udd70-\udd71]|\ud83c[\udd7e-\udd7f]|\ud83c\udd8e|\ud83c[\udd91-\udd9a]|\ud83c[\udde6-\uddff]|[\ud83c[\ude01-\ude02]|\ud83c\ude1a|\ud83c\ude2f|[\ud83c[\ude32-\ude3a]|[\ud83c[\ude50-\ude51]|\u203c|\u2049|[\u25aa-\u25ab]|\u25b6|\u25c0|[\u25fb-\u25fe]|\u00a9|\u00ae|\u2122|\u2139|\ud83c\udc04|[\u2600-\u26FF]|\u2b05|\u2b06|\u2b07|\u2b1b|\u2b1c|\u2b50|\u2b55|\u231a|\u231b|\u2328|\u23cf|[\u23e9-\u23f3]|[\u23f8-\u23fa]|\ud83c\udccf|\u2934|\u2935|[\u2190-\u21ff])/g;
   const regex = /([\uE000-\uF8FF]|\uD83C[\uDC00-\uDFFF]|\uD83D[\uDC00-\uDFFF]|[\u2011-\u26FF]|\uD83E[\uDD10-\uDDFF])/g;
+  var re = new RegExp("`", "gi");
   const unicodeIndices = [];
-  while ((match = regex.exec(full_text)) != null) {
+
+  const matches = full_text.match(regex);
+  full_text = full_text.replace(regex, "`");
+  while ((match = re.exec(full_text)) != null) {
     unicodeIndices.push(match.index);
   }
-  const matches = full_text.match(regex);
-  full_text = full_text.replace(regex, " ");
 
   const bodies = [];
+  // console.log(unicodeIndices);
+  // console.log(ents.map(e => e.indices));
+  // console.log(full_text.length);
   const lastIndex = ents.reduce((index, ent, i) => {
     const { indices } = ent;
-
     let start = "";
-    console.log(matches);
     const curr = unicodeIndices.reduce((curr, position, i) => {
-      console.log(curr, position, curr <= position && position < indices[0]);
       if (curr <= position && position < indices[0]) {
         start += full_text.substring(curr, position) + matches[i];
         return position + 1;
@@ -106,7 +109,6 @@ const TwitterItem = ({ item, navigation }) => {
       }
     }, index);
     start += full_text.substr(curr, indices[0] - curr);
-    console.log(start);
 
     let replacement = null;
     if (ent.text)
@@ -141,7 +143,17 @@ const TwitterItem = ({ item, navigation }) => {
     if (replacement) bodies.push(replacement);
     return indices[1];
   }, 0);
-  bodies.push(full_text.substr(lastIndex));
+
+  const curr = unicodeIndices.reduce((curr, position, i) => {
+    if (lastIndex <= position) {
+      bodies.push(full_text.substring(curr, position) + matches[i]);
+      return position + 1;
+    } else {
+      return curr;
+    }
+  }, lastIndex);
+
+  bodies.push(full_text.substr(curr));
 
   return (
     <View style={{ backgroundColor: theme.bg2(), padding: theme.spacing_3 }}>
